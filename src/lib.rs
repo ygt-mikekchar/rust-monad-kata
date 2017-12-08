fe0c4a3cb9e37720a4f8b49be53ef2ae96431e8a
Tom Johnson <tom@tmjohnson.co.uk>
2017-12-08 10:25:02 +0000

    Complete step one

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn first_seed() {
+        assert_eq!(rand(1), (1, 2));
+        assert_eq!(rand(5), (5, 6));
+    }
+
+}
+
+type Seed = u32;
+
+fn rand(seed: Seed) -> (u32, Seed) {
+  return (seed, seed + 1);
+}
+
