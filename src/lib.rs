2e500970809bca1e764772272ef586f3101a6b9c
Tom Johnson <tom@tmjohnson.co.uk>
2017-12-08 11:05:21 +0000

    Completed five_rands

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,26 @@ mod tests {
         assert_eq!(rand(5), (5, 6));
     }
 
+    #[test]
+    fn test_five_rands() {
+        assert_eq!(five_rands(), vec!(1, 2, 3, 4, 5));
+    }
+
 }
 
 type Seed = u32;
 
 fn rand(seed: Seed) -> (u32, Seed) {
   (seed, seed + 1)
+}
+
+fn five_rands() -> Vec<u32> {
+    let mut seed = 1;
+    let rands : Vec<u32> = (0..5).collect();
+
+    rands.iter().map(|_i| {
+        let (val, new_seed) = rand(seed);
+        seed = new_seed;
+        val
+    }).collect()
 }
