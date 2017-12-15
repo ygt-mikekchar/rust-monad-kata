16ecb472f5f778999838686ca05dcf3526d814bd
Tom Johnson <tom@tmjohnson.co.uk>
2017-12-15 14:30:45 +0000

    Implemented step 8

--- src/lib.rs
+++ src/lib.rs
@@ -13,21 +13,21 @@ mod tests {
         assert_eq!(rand(5), (5, 6));
     }
 
-    // #[test]
-    // fn test_rand_letter() {
-    //     assert_eq!(rand_letter(1), ('a', 2));
-    //     assert_eq!(rand_letter(5), ('e', 6));
-    // }
+    #[test]
+    fn test_rand_letter() {
+        assert_eq!(rand_letter(1), ('a', 2));
+        assert_eq!(rand_letter(5), ('e', 6));
+    }
 
     #[test]
     fn test_five_rands() {
         assert_eq!(five_rands(), vec!(1, 2, 3, 4, 5));
     }
 
-    // #[test]
-    // fn test_three_rand_letters() {
-    //     assert_eq!(three_rand_letters(), "abc");
-    // }
+    #[test]
+    fn test_three_rand_letters() {
+        assert_eq!(three_rand_letters(), "abc");
+    }
 
     #[test]
     fn test_even_rands() {
@@ -42,33 +42,35 @@ mod tests {
 
 type Seed = u32;
 
-type Rand = (u32, Seed);
+type Rand<T> = (T, Seed);
 
-trait Functor {
-    fn map(&self, f: fn(u32) -> u32) -> Self;
+trait Functor<T, U> {
+    type Output;
+    fn map(self, f: fn(T) -> U) -> Self::Output;
 }
 
-impl Functor for Rand {
-    fn map(&self, f: fn(u32) -> u32) -> Rand {
+impl <T, U> Functor<T, U> for Rand<T> {
+    type Output = Rand<U>;
+    fn map(self, f: fn(T) -> U) -> Rand<U> {
         (f(self.0), self.1)
     }
 }
 
-fn rand(seed: Seed) -> Rand {
+fn rand(seed: Seed) -> Rand<u32> {
     (seed, seed + 1)
 }
 
-fn rand_even(seed: Seed) -> Rand {
+fn rand_even(seed: Seed) -> Rand<u32> {
     rand(seed).map(|v| v * 2)
 }
 
-fn rand_odd(seed: Seed) -> Rand {
+fn rand_odd(seed: Seed) -> Rand<u32> {
     rand(seed).map(|v| v * 2 + 1)
 }
 
-// fn rand_letter(seed: Seed) -> Rand<char> {
-//     rand_map(|val| i_to_a(val), rand(seed))
-// }
+fn rand_letter(seed: Seed) -> Rand<char> {
+    rand(seed).map(|v| i_to_a(v))
+}
 
 fn i_to_a(val: u32) -> char {
     let a = 'a' as u32;
@@ -89,13 +91,13 @@ fn five_rands() -> Vec<u32> {
     }).collect()
 }
 
-// fn three_rand_letters() -> String {
-//     let mut seed = 1;
-//     let rands : Vec<u32> = (0..3).collect();
+fn three_rand_letters() -> String {
+    let mut seed = 1;
+    let rands : Vec<u32> = (0..3).collect();
 
-//     rands.iter().map(|_i| {
-//         let (val, new_seed) = rand_letter(seed);
-//         seed = new_seed;
-//         val
-//     }).collect()
-// }
+    rands.iter().map(|_i| {
+        let (val, new_seed) = rand_letter(seed);
+        seed = new_seed;
+        val
+    }).collect()
+}
